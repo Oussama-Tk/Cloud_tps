0436b3d9ca5c8ca79402a49c748dc5db820174a6
oussama tkitak <tkitakoussama8@gmail.com>
2026-03-02 14:53:41 +0000

    yy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json()) ;
 
 const db = mysql.createConnection({
     host : 'localhost' ,
-    database : 'gestion_stagiaire' ,
+    database : 'gestion_reservation' ,
     user : 'root' ,
     password : ''
 })
@@ -19,117 +19,89 @@ db.connect((err) => {
     }
 });
 
+/// I. Requêtes CRUD de base (Rappel)
 
-/// 2.	Créer une API pour insérer un nouvel stagiaire. Vous devez vérifier d’abord l’existence du groupe dans la base de données.
 
-app.post('/stagiaire/add' , (req , res)=>{
-    const {nom , ville , id_group} = req.body ;
-    db.query('SELECT * FROM groupe WHERE id = ?' , [id_group] , (err , groupe_exist) => {
-        if(err) throw err ;
-        if(groupe_exist.length === 0){res.status(404).json({
-            message : 'groupe n\'existe pas' 
-        })} ;
 
-        db.query('INSERT INTO stagiaire (nom , ville , id_group) VALUES (? , ? , ?)' ,[nom , ville , id_group] , (err , result) => {
-            if(err) throw err ;
-            res.json({
-                message : 'Stagiaire ajoutee avec success !'
-            })
-        })
+/// 1.	Implémentez les routes CRUD pour les clients.
 
-    });
-})
+//// index : 
 
-/// 3.	Créer une API pour modifier la ville des stagiaires du groupe dont l’id est donné en paramètre.
-
-app.put('/stagiaire/update_ville/:group_id', (req , res)=> {
-    const {group_id} = req.params ;
-    const {ville} = req.body ;
-
-    db.query('UPDATE stagiaire SET ville = ? WHERE group_id = ?' ,[ville , group_id] , (err,result)=>{
-        if(err) throw err ;
-        res.json({message : 'Stagiaire Updated successfully'}) ;
-    });
-});
-
-/// 4.	Créer une API pour supprimer les formateurs dont la ville est donnée en paramètre.
-
-app.delete('/stagiaire/:ville' , (req , res) => {
-    const {ville} = req.params ;
-    db.query('DELETE FROM fourmateur WHERE ville = ?' ,[ville] , (err , result)=>{
-        if(err) throw err ;
-        res.json({message : 'Stagiaire deleted successfully'}) ;
-    });
-});
-
-/// 5.	Créer une API pour afficher la liste de tous les stagiaires trier par id groupe.
-
-app.get('/stagiaire/groupTrie' , (req , res)=> {
-    db.query('SELECT * FROM stagiaire ORDER BY id_group ' , (err , result) => {
+app.get('/clients' , (req , res)=> {
+    db.query('SELECT * FROM clients' , (err , result) => {
         if(err) throw err ;
         res.json(result) ;
     }) ;
 }) ;
 
-/// 6.	Créer une API pour afficher le stagiaire par id.
+//// show :
 
-app.get('/stagiaire/:id' , (req , res) => {
-    const {id} = req.params ;
-    db.query('SELECT * FROM stagiaire WHERE id = ?' , [id] , (err , result) => {
+app.get('/clients/:id' , (req , res)=> {
+    const {id} = req.body ;
+    db.query('SELECT * FROM clients WHERE id = ?' , [id] , (err , result) => {
         if(err) throw err ;
-        res.json(result[0]) ;
+        res.json(result) ;
+    }) ;
+}) ;
+
+//// create :
+app.post('/clients/add' , (req , res)=>{
+    const {nom , email , telephone} = req.body ;
+
+    db.query('INSERT INTO clients (nom , email , telephone) VALUES (? , ? , ?)' ,[nom , email , telephone] , (err , result) => {
+        if(err) throw err ;
+        res.json({
+            message : 'Client ajoutee avec success !'
+        })
     })
-})
 
-/// 7.	Créer une API pour afficher la liste des stagiaires par nom du groupe. 
+});
 
-app.get('/stagiaire/group/:nom' , (req , res) => {
-    const {nom} = req.params ;
-    db.query('SELECT s.id, s.nom, s.ville, g.nom AS nom_groupe FROM stagiaire  s JOIN groupe g ON s.id_group = g.id WHERE g.nom = ?' , [nom] , (err , result) => {
+//// update :
+
+app.post('/clients/:id/update' , (req , res)=>{
+    const {id} = req.params ;
+    const {nom , email , telephone} = req.body ;
+
+    db.query('UPDATE clients SET (nom , email , telephone) VALUES (? , ? , ?) WHERE id = ?' ,[nom , email , telephone , id] , (err , result) => {
         if(err) throw err ;
-        res.json(result) ;
-    }) ;
-} );
+        res.json({
+            message : 'Client updated avec success !'
+        })
+    })
 
-/// 8.	Créer une API pour afficher la liste des stagiaires par nom du formateur.
+});
 
-app.get('/stagiaire/formateur/:nom' , (req , res) => {
-    const {nom} = req.params ;
-    db.query('SELECT s.id, s.nom, s.ville, f.nom AS formateur FROM stagiaire  s JOIN fourmateur f ON f.id_group = s.id_group WHERE f.nom = ?' , [nom] , (err , result) => {
+//// Delete :
+
+app.delete('/clients/:id/delete' , (req , res) => {
+    const {id} = req.params ;
+    db.query('DELETE FROM fourmateur WHERE id = ?' ,[id] , (err , result)=>{
         if(err) throw err ;
-        res.json(result) ;
-    }) ;
-} );
+        res.json({message : 'Client deleted successfully'}) ;
+    });
+});
 
-/// 9.	Créer une API pour afficher la liste des stagiaires qui étudient le module dont le nom est donné en paramètre.
 
-app.get('/stagiaire/module/:nom' , (req , res) => {
-    const {nom} = req.params ;
-    db.query('SELECT s.id, s.nom, s.ville, m.nom AS module FROM stagiaire  s JOIN fourmateur f ON f.id_group = s.id_group JOIN module m ON f.id_module = m.id WHERE m.nom = ?' , [nom] , (err , result) => {
+/// 2.	Implémentez une route SQL pour  insérer une réservation
+
+app.post('/reservation/add' , (req , res)=>{
+    const {client_id  , chambre_id , date_arrivee , date_depart , statut} = req.body ;
+
+    db.query('SELECT disponible FROM chambre WHERE id = ?' , [chambre_id] , (err , disponible) => {
         if(err) throw err ;
-        res.json(result) ;
-    }) ;
-} );
 
-/// 10.	Créer une API pour afficher la liste des formateurs qui enseignent le module dont le nom est donné en paramètre.
-
-app.get('/formateur/module/:nom' , (req , res) => {
-    const {nom} = req.params ;
-    db.query('SELECT f.id, f.nom, f.ville, m.nom AS module FROM fourmateur f JOIN module m ON f.id_module = m.id WHERE m.nom = ?' , [nom] , (err , result) => {
+        res.json(disponible[0])
+    });
+    if()
+    db.query('INSERT INTO clients (client_id  , chambre_id , date_arrivee , date_depart , statut ) VALUES (? , ? , ?)' ,[client_id  , chambre_id , date_arrivee , date_depart , statut] , (err , result) => {
         if(err) throw err ;
-        res.json(result) ;
-    }) ;
-} );
+        res.json({
+            message : 'Reservation ajoutee avec success !'
+        })
+    })
 
-/// 11.	Créer une API pour afficher le nombre de stagiaires par id formateur et par id module.
-
-app.get('/stagiaire/nb' , (req , res) => {
-    db.query('SELECT COUNT(*) , f.nom AS formateur FROM stagiaire  s JOIN fourmateur f ON f.id_group = s.id_group  GROUP BY f.id , f.id_module' , (err , result) => {
-        if(err) throw err ;
-        res.json(result) ;
-    }) ;
-} );
-
+});
 
 
 
